f3f49e8e59a03e417e72dd7a4a3395f21d7c2aa9
gm <550146647@qq.com>
2020-12-08 22:12:53 +0800

    js start

---
+++ 1.networkProgramming/4.html/1.前端/2.javascript/1.基本语法/2.myScript.js
@@ -0,0 +1,6 @@
+function myFunction4() {
+   document.getElementById("demo4").innerHTML = "4.javascript的函数也可以写在html文件外，因为可能其他html文件也会用到。";
+}
+function myFunction5() {
+   document.getElementById("demo5").innerHTML = "5.javascript的函数也可以写在html文件外，这个src也可以是url，可以是相对位置";
+}
