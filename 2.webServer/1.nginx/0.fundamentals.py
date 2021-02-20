e7ff7aab7c417687f0afdd76191f427ea9c4f95f
gm <550146647@qq.com>
2021-02-20 15:46:18 +0800

    added

--- 2.webServer/1.nginx/0.fundamentals.py
+++ 2.webServer/1.nginx/0.fundamentals.py
@@ -5,5 +5,12 @@
     nginx -t
 3.重启服务
     nginx -s reload
-4.
+4.查看服务
+    ps axw -o pid,ppid,user,%cpu,vsz,wchan,command | egrep '(nginx|PID)'
+"""
+"""
+文件：
+    html文件在/usr/local/Cellar/nginx/1.19.6
+    conf文件在/usr/local/etc/nginx
+    log文件在/usr/local/var/log/nginx
 """
