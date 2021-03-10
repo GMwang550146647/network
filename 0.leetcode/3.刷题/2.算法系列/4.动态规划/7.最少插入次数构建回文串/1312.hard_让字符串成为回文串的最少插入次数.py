dbcafe59d49448329cf97d6fd608be0be69bcadf
awang999233 <gmwang_global@qq.com>
2021-03-10 16:14:02 +0800

    added

---
+++ 0.leetcode/3.刷题/2.算法系列/4.动态规划/7.最少插入次数构建回文串/1312.hard_让字符串成为回文串的最少插入次数.py
@@ -0,0 +1,22 @@
+from fundamentals.test_time import test_time
+
+import bisect
+import numpy as np
+
+
+class Solution():
+    def __init__(self):
+        pass
+
+    @test_time
+    def minInsertions(self, s):
+
+
+    def main(self):
+        s = "mbadm"
+        self.minInsertions(s)
+
+
+if __name__ == '__main__':
+    SL = Solution()
+    SL.main()
