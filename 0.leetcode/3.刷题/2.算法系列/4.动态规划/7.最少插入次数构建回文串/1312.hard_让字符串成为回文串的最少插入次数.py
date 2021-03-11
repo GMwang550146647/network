eb99cbc6c6a0e3665708d2fbd68e574a8a97a20a
awang999233 <gmwang_global@qq.com>
2021-03-11 17:34:51 +0800

    added

--- 0.leetcode/3.刷题/2.算法系列/4.动态规划/7.最少插入次数构建回文串/1312.hard_让字符串成为回文串的最少插入次数.py
+++ 0.leetcode/3.刷题/2.算法系列/4.动态规划/7.最少插入次数构建回文串/1312.hard_让字符串成为回文串的最少插入次数.py
@@ -9,12 +9,80 @@ class Solution():
         pass
 
     @test_time
-    def minInsertions(self, s):
+    def minInsertions_recur_record(self, s):
+        """
+        recursion dp +record_dict
+        """
+        dp_table = {}
 
+        def dp(i, j):
+            if i >= j:
+                return 0
+            elif (i, j) in dp_table:
+                dp_table[(i + 1, j - 1)] = dp_table[(i, j)]
+            elif s[i] == s[j]:
+                dp_table[(i, j)] = dp(i + 1, j - 1)
+            else:
+                dp_table[(i, j)] = min(dp(i + 1, j), dp(i, j - 1)) + 1
+            return dp_table[(i, j)]
+
+        return dp(0, len(s) - 1)
+
+    @test_time
+    def minInsertions_dp(self, s):
+        """
+        iteration dp
+        """
+        len_s = len(s)
+        dp = [[0 for _ in range(len_s)] for _ in range(len_s)]
+        for i in range(len_s - 1, -1, -1):
+            for j in range(i + 1, len_s):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = min(dp[i][j - 1], dp[i + 1][j]) + 1
+        return dp[0][-1]
+
+    @test_time
+    def minInsertions_dp_compress_list(self, s):
+        """
+        iteration dp + compression( addition list dp_cpy)
+        """
+        len_s = len(s)
+        dp = [0 for _ in range(len_s)]
+        for i in range(len_s - 1, -1, -1):
+            dp_cpy = dp.copy()
+            for j in range(i + 1, len_s):
+                if s[i] == s[j]:
+                    dp[j] = dp_cpy[j - 1]
+                else:
+                    dp[j] = min(dp[j - 1], dp[j]) + 1
+        return dp[-1]
+
+    @test_time
+    def minInsertions_dp_compress_var(self, s):
+        """
+        iteration dp + compression (addition var pre)
+        """
+        len_s = len(s)
+        dp = [0 for _ in range(len_s)]
+        for i in range(len_s - 1, -1, -1):
+            pre =0
+            for j in range(i + 1, len_s):
+                tempt=dp[j]
+                if s[i] == s[j]:
+                    dp[j] = pre
+                else:
+                    dp[j] = min(dp[j - 1], dp[j]) + 1
+                pre=tempt
+        return dp[-1]
 
     def main(self):
         s = "mbadm"
-        self.minInsertions(s)
+        self.minInsertions_recur_record(s)
+        self.minInsertions_dp(s)
+        self.minInsertions_dp_compress_list(s)
+        self.minInsertions_dp_compress_var(s)
 
 
 if __name__ == '__main__':
---
+++ 0.leetcode/3.刷题/2.算法系列/4.动态规划/8.正则表达式/10.hard_正则表达式匹配.py
@@ -0,0 +1,69 @@
+from fundamentals.test_time import test_time
+
+
+class Solution():
+    def __init__(self):
+        pass
+
+    # @test_time
+    def isMatch_recur_record(self, s, p):
+        """
+        比较难的版本
+        """
+
+        @test_time
+        def dp(i=0, j=0):
+            ###1.base case
+            # pattern到头了，
+            if j == len_p:
+                return i == len_s
+            # 字符串到头了（如果pattern还有，只能是*的情况）
+            if i == len_s:
+                if ((len_p - j) % 2 == 1):
+                    return False
+                else:
+                    for k in range(j + 1, len_p, 2):
+                        if p[k] != '*':
+                            return False
+                    return True
+            ###2.in dict
+            if (i, j) in dp_table:
+                return dp_table[(i, j)]
+            ###3.dp
+            else:
+                if p[j] in [s[i], '.']:
+                    # 有*
+                    if j < len_p - 1 and p[j + 1] == '*':
+                        res = dp(i + 1, j) | dp(i, j + 2)
+                    # 无*
+                    else:
+                        res = dp(i + 1, j + 1)
+                # 匹配失败(但是可能有*)
+                else:
+                    # 有*
+                    if j < len_p - 1 and p[j + 1] == '*':
+                        res = dp(i, j + 2)
+                    # 冇救了
+                    else:
+                        res = False
+                dp_table[(i, j)] = res
+                return res
+
+        len_s = len(s)
+        len_p = len(p)
+        dp_table = {}
+        return dp()
+
+    def isMatch_dp(self, s, p):
+        pass
+
+    def main(self):
+        s = "mississippi"
+        p = "mis*is*p*."
+        self.isMatch_recur_record(s, p)
+        self.isMatch_dp(s, p)
+
+
+if __name__ == '__main__':
+    SL = Solution()
+    SL.main()
