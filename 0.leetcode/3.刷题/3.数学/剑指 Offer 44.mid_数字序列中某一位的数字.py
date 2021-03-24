01671f494db6f52aa0025aaf9c4df545b7282130
awang999233 <gmwang_global@qq.com>
2021-03-24 18:02:18 +0800

    addeed

--- 0.leetcode/3.刷题/3.数学/剑指 Offer 44.mid_数字序列中某一位的数字.py
+++ 0.leetcode/3.刷题/3.数学/剑指 Offer 44.mid_数字序列中某一位的数字.py
@@ -3,14 +3,17 @@ from fundamentals.test_time import test_time
 
 class Solution():
     def __init__(self):
-        pass
-
+        self.divs = [0,10]
+        i = 1
+        while self.divs[-1] < 2 ** 21:
+            self.divs.append((i + 1) * 9 * 10 ** i)
+            i += 1
     @test_time
     def findNthDigit(self, n):
-        divs=[10]
-        i=1
-        while divs[-1]<2**21:
-            divs.append(9*10**i*)
+        len_n=len(str(n))
+
+
+        return divs
 
     def main(self):
         n=100
